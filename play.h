4e45e8685dd77e872058db8d5445c921926d008f
Nepranal <99369599+Nepranal@users.noreply.github.com>
2022-05-02 21:57:09 +0800

    Delete play.h

--- play.h
+++
@@ -1,8 +0,0 @@
-#ifndef PLAY_H
-#define PLAY_H
-
-using namespace std;
-void play(int space, string board[][50], vector<vector<int>> &snake_position, int foodposition[], int& score, string gamemode);
-void hard_play(int space, string board[][50], vector<vector<int>> &snake_position, int foodposition[], int& score, string gamemode, int poisonposition[], int poison[][50], int fruitposition[]);
-
-#endif
