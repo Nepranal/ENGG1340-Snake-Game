a5bf8c230d4319d247c68a6d806c10307c433ff2
Nepranal <99369599+Nepranal@users.noreply.github.com>
2022-05-02 21:56:08 +0800

    Delete movebody.h

--- movebody.h
+++
@@ -1,6 +0,0 @@
-#ifndef MOVEBODY_H
-#define MOVEBODY_H
-
-void movebody(int i,int prevpos[],int snakelength, std::vector<std::vector<int>> &snake_position,std::string board[][50], int space);
-
-#endif
