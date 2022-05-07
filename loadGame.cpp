d35fc5a1482357ac1668a6f795fcec3e0b9b336c
Nepranal <99369599+Nepranal@users.noreply.github.com>
2022-05-07 22:19:20 +0800

    Updated comments

--- loadGame.cpp
+++ loadGame.cpp
@@ -5,7 +5,8 @@ using namespace std;
 #include "loadGame.h"
 #include "play.h"
 
-// It loads up the game.
+// It loads up the game with option "5" a special option to load saved games.
+// Takes no arguments and return no arguments.
 void loadGame()
 {
     play(5); //5 is an abritrary int argument. Any number except [1,4] is acceptable. It is only to activate the getParameter function in play()
